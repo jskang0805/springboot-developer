e50224884c0a75f4fe770171924b91f42d1336a2
jskang0805 <jskang3560@gmail.com>
2024-01-03 20:50:29 +0900

    project init

---
+++ src/main/java/me/kangjisun/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,15 @@
+package me.kangjisun.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+
+
+}
